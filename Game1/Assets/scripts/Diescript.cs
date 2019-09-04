c47bae52e794fb2febbd3b1ba659374c859acab3
QinamruG <52399739+QinamruG@users.noreply.github.com>
2019-09-04 23:54:00 +0300

    v1.045
    new levels + optimization+ bug fixes

---
+++ Game1/Assets/scripts/Diescript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diescript : MonoBehaviour
+
+{
+    public GameObject respawn;
+    private void OnTriggerEnter2D(Collider2D  other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.position = respawn.transform.position;
+
+        }
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
