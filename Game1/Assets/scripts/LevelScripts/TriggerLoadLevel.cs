9c2e268d236a45d23bb648283ce370493297348f
QinamruG <52399739+QinamruG@users.noreply.github.com>
2019-09-05 23:11:31 +0300

    v1.051
    -added enemies(small bugs) and lives

--- Game1/Assets/scripts/LevelScripts/TriggerLoadLevel.cs
+++ Game1/Assets/scripts/LevelScripts/TriggerLoadLevel.cs
@@ -9,7 +9,16 @@ public class TriggerLoadLevel : MonoBehaviour
     public int Level;
 
 
+    void Start()
+    {
+        
+    }
 
+    
+    void Update()
+    {
+        
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
